b598decab3faf2ddda380326d8fa3e15204b0968
OukhtySama <allanv69330@gmail.com>
2023-03-09 17:08:12 +0100

    Exo4

--- Documents/ESGI/dev/exoJS/exo3.js
+++ Documents/ESGI/dev/exoJS/exo3.js
@@ -1,9 +1,11 @@
 //Créez un tableau contenant les nombres pairs de 2 à 10. Écrivez un programme qui calcule la somme des nombres du tableau et l'affiche à la console. 
 
-let monTableau = [2, 4, 6, 8, 10]
-let sum = 0
-for (let index = 0; index < monTableau.length; index++) {
-    sum += monTableau[index]
+let monTableau = [2, 5, 8, 7, 6]
 
+reduce = 0
+
+for (let index = 0; index < monTableau.length; index++) {
+    reduce -= monTableau[index]
+    
 }
-console.log(sum)
+console.log(reduce)
